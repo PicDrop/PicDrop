5d004dd3284ae623e31fb1ed6b8959dbc3d8cdf4
Ryan Jagger <ryanjagger@gmail.com>
2016-01-23 18:47:27 -0600

    Node/Express initial configuration with routing

---
+++ server/controllers/api/user.controller.js
@@ -0,0 +1,12 @@
+// require database user model
+var User = require('../../models/userModel');
+
+module.exports = {
+  createDrop: function(req, res){},
+  getDrops: function(req, res){},
+  getDrop: function(req, res){},
+  starDrop: function(req, res){},
+  tagDrop: function(req, res){},
+  getCategory: function(req, res){},
+  getTagname: function(req, res){},
+}  
