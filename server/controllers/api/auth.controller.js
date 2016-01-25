a6362ce9390a3dac4c7788088f74a6f1cdba0968
Bryan Bierce <bryanbierce@gmail.com>
2016-01-24 18:59:11 -0800

    Moved models into common object

--- server/controllers/api/auth.controller.js
+++ server/controllers/api/auth.controller.js
@@ -2,13 +2,11 @@
 var User = require('../../models/userModel');
 
 module.exports = {
-
-userCreate: function(req, res){
-  console.log('auth userCreate called');
-  res.send('User Created - Test Only');
-},
-userLogin: function(req, res){},
-userLogout: function(req, res){},
-userFacebookLogin: function(req, res){},
-
+  userCreate: function(req, res){
+    console.log('auth userCreate called');
+    res.send('User Created - Test Only');
+  },
+  userLogin: function(req, res){},
+  userLogout: function(req, res){},
+  userFacebookLogin: function(req, res){},
 }
