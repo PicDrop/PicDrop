275bf461c22d8b1cd4afb4f1d7aff81053f4e2c9
Bryan Bierce <bryanbierce@gmail.com>
2016-01-25 16:37:55 -0800

    Configured Webpack for extension and client

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: {
+    'client/bundle': [
+      'webpack-dev-server/client?http://localhost:8080',
+      'webpack/hot/only-dev-server',
+      './client/Main.js',
+    ],
+    'extension/bundle': './extension/extension.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'react-hot!babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/public/',
+    publicPath:'/client',
+    filename: '[name].js'
+  },
+  devServer: {
+    contentBase: './public',
+    hot: true
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
+};
