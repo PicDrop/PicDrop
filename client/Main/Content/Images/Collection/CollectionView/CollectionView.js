dd1ff3771d09b051ad8541054525ceabe432f494
Santosh Gautam <santoshgautam7@gmail.com>
2016-01-26 15:52:12 -0800

    Implemented CollectionView.

---
+++ client/Main/Content/Images/Collection/CollectionView/CollectionView.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class CollectionView extends React.Component {
+  render() {
+    return (
+      <div>CollectionView</div>
+    );
+  }
+}
+
+export default CollectionView;
