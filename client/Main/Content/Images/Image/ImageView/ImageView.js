858ef7505e34d31512fad0c3c534ac907a2f0eaf
Ryan Jagger <ryanjagger@gmail.com>
2016-01-26 16:11:31 -0600

    Component setup for ImageInfo and ImageView

--- client/Main/Content/Images/Image/ImageView/ImageView.js
+++ client/Main/Content/Images/Image/ImageView/ImageView.js
@@ -1,9 +1,17 @@
+import React from 'react';
+import FlatButton from 'material-ui/lib/flat-button';
+import Paper from 'material-ui/lib/paper';
+
 class ImageView extends React.Component({
-  render(){
+  render: function(){
     return (
-      <DownloadButton />
-      <Picture />
-      <DeleteButton />
+      <div>
+        <FlatButton style={border: 'solid', border-width: 'thin'} label="Download" />
+        <Paper style={height: 500, width: 500, margin: 20, textAlign: 'center', display: 'inline-block'} zDepth={1}/>
+      </div>
+
     );
   }
-});
+})
+
+export default ImageView
