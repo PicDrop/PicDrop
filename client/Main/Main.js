6647041a3c00aaf4e07c0f1ff73ceaef230270e1
Bryan Bierce <bryanbierce@gmail.com>
2016-01-27 09:39:07 -0800

    Amended merge

--- client/Main/Main.js
+++ client/Main/Main.js
@@ -1,6 +1,3 @@
-<<<<<<< 8505aef0c09041b1744d916190d6d42ac7638c8e
-// Second level container, renders nav bar and third level container
-=======
 import React from 'react';
 import NavBar from './NavBar/Navbar';
 import Content from './Conent/Content';
@@ -17,4 +14,3 @@ class Main extends React.Component {
 }
 
 export default Main;
->>>>>>> Store implemented with defaultState and masterReducer
