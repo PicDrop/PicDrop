0f61bbd1e9b4a83d52b40403489b746b463c29b9
Santosh Gautam <santoshgautam7@gmail.com>
2016-01-26 16:03:03 -0800

    Implemented collectionTags.

---
+++ client/Main/Content/Images/Collection/CollectionInfo/Tags/Tags.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Tags extends React.Component {
+  render() {
+    return (
+      <div>
+        <h3>Tags</h3>
+        <ul>
+          <li>Clean</li>
+          <li>I Want</li>
+          <li>Beds</li>
+        </ul>
+        <a href="#">Show All Tags</a>
+      </div>
+    );
+  }
+}
+
+export default Tags;
