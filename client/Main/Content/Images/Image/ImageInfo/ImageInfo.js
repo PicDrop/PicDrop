ddd0cfe7af9bba61ebd3059f36694f9e3500911a
Ryan Jagger <ryanjagger@gmail.com>
2016-01-26 19:03:03 -0600

    Added some additional Material UI components

--- client/Main/Content/Images/Image/ImageInfo/ImageInfo.js
+++ client/Main/Content/Images/Image/ImageInfo/ImageInfo.js
@@ -1,17 +1,38 @@
 import React from 'react';
+import FlatButton from 'material-ui/lib/flat-button';
+import TextField from 'material-ui/lib/text-field';
 
 
 class ImageInfo extends React.Component({
   render: function(){
     return (
       <div>
-        <Title />
-        <Status />
-        <Domain />
-        <Folder />
-        <Tags />
-        <DeleteButton />
+        <div>
+          <FlatButton style={border: 'solid', border-width: 'thin'} label="Download" />
+        </div>
+        <div id="metadata">
+          title
+          status
+          saved to drive
+          domain
+          folder
+        </div>
+        <div id="tags">
+          <TextField hintText="Tags..." underlineFocusStyle={{borderColor: #ED1E79}} />
+          <FlatButton label="Clean" />
+          <FlatButton label="Login" />
+          <FlatButton label="Materialize" />
+        </div>
+        <div></div>
+
+
       </div>
+
+
+
+
+
+
 
     );
   }
