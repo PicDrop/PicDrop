f0a1e342c5342c2f8a86dc4d781a66cae6606025
Ryan Jagger <ryanjagger@gmail.com>
2016-01-26 19:03:03 -0600

    Added some additional Material UI components

--- client/Main/Content/Images/Image/Image.js
+++ client/Main/Content/Images/Image/Image.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import ImageInfo from './ImageInfo/ImageInfo';
+import ImageView from './ImageView/ImageView';
+
 
 class Image extends React.Component({
   render: function(){
