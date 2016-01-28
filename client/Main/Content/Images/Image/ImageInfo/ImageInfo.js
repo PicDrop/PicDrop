cb578f4f25fd46cf77a5f78c2aa7b31e0f7cce2d
Bryan Bierce <bryanbierce@gmail.com>
2016-01-27 16:32:39 -0800

    Removed left over old structure

--- client/Main/Content/Images/Image/ImageInfo/ImageInfo.js
+++ client/Main/Content/Images/Image/ImageInfo/ImageInfo.js
@@ -4,6 +4,22 @@ import TextField from 'material-ui/lib/text-field';
 
 
 class ImageInfo extends React.Component {
+  addTag(){
+    //take current tags
+    //check for existance of this tag
+      //if exists add picId
+        //pass that updated tag object
+      //if doesn't exist
+        //build new tag object
+        //pass new tag object to tagsAction.addTag
+
+
+    //take current tags
+    //merge tagname with id
+      //pass all tags to tagsAction.addTag
+
+  }
+
   render() {
     return (
       <div>
