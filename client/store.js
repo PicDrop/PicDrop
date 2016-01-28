4893f9bce1d070e866d19f50a1ce83641f37b1a7
Bryan Bierce <bryanbierce@gmail.com>
2016-01-27 21:15:53 -0800

    Replaced defaultState with defaultStateImmut

--- client/store.js
+++ client/store.js
@@ -1,10 +1,7 @@
 import { createStore } from 'redux';
-import Immutable from 'immutable';
 import reducer from './reducers/index';
 import appActions from './actions/appActions';
 import defaultState from './defaultState';
-
-const defaultStateImmut = Immutable.fromJS(defaultState);
 
 const store = createStore(reducer);
 store.dispatch(appActions.setState(defaultStateImmut));
