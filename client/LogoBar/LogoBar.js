5b646a4a5398e845f352569a85fdbb30b49236e1
Ryan Jagger <ryanjagger@gmail.com>
2016-01-29 23:37:55 -0600

    Changed logobar and navbar to material ui components

--- client/LogoBar/LogoBar.js
+++ client/LogoBar/LogoBar.js
@@ -2,20 +2,26 @@ import React from 'react';
 import Logo from './Logo';
 import LogInOut from './LogInOut';
 import AppBar from 'material-ui/lib/app-bar';
+<<<<<<< 04bc5a597a8dd39407c00fbd42a7b9af8e80217f
 import ActionFlightTakeoff from 'material-ui/lib/svg-icons/action/flight-takeoff';
 
 
 const REDBORDERSTYLE = {borderStyle: 'dotted', borderColor: 'red'};
+=======
+
+>>>>>>> Changed logobar and navbar to material ui components
 
 class LogoBar extends React.Component {
   render() {
     return (
-      <AppBar
-        style={{ backgroundColor: 'WHITE' }}
-        title={<span style={{ color: 'DIMGREY', fontFamily: 'Abel, sans-serif' }}>PICDROP</span>}
-        iconElementRight={<LogInOut history={this.props.history} />}
-        zDepth={0}
-      />
+      <div>
+        <AppBar
+          style={{ backgroundColor: 'WHITE' }}
+          title={<span style={{ color: 'DIMGREY', fontFamily: 'Abel, sans-serif' }}>PICDROP</span>}
+          iconElementRight={<LogInOut history={this.props.history} />}
+          zDepth={0}
+        />
+      </div>
     );
   }
 }
