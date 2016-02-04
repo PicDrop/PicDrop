60df6ea8cc14eb089e5b30fbe9e00f02d3890c39
Ryan Jagger <ryanjagger@gmail.com>
2016-02-03 18:24:23 -0600

    Configured Back button on ImageInfo; code cleanup

--- client/Main/Content/Images/Image/ImageInfo/ImageInfo.js
+++ client/Main/Content/Images/Image/ImageInfo/ImageInfo.js
@@ -5,10 +5,11 @@ import TextField from 'material-ui/lib/text-field';
 import List from 'material-ui/lib/lists/list';
 import ListItem from 'material-ui/lib/lists/list-item';
 
-const SUBHEADERSTYLE = {fontSize: '11px', lineHeight: '10px'};
-const REDBORDERSTYLE = {borderStyle: 'dotted', borderColor: 'red'};
+const SUBHEADERSTYLE = { fontSize: '11px', lineHeight: '10px' };
+const REDBORDERSTYLE = { borderStyle: 'dotted', borderColor: 'red' };
 
 class ImageInfo extends React.Component {
+
   addTag() {
     //take current tags
     //check for existance of this tag
@@ -29,31 +30,37 @@ class ImageInfo extends React.Component {
     return (
       <div>
           <div>
-            <FlatButton label="Back" hoverColor="WHITE"/>
+            <FlatButton
+              onTouchTap={ this.props.history.push.bind(this, { pathname: '/main/collection' }) }
+              label="Back"
+              hoverColor="WHITE"
+              disableTouchRipple={true}
+            />
           </div>
           <div id="metadata">
             <List subheader="Title" subheaderStyle={ SUBHEADERSTYLE } >
-              <ListItem primaryText={ this.props.title } disabled={true} />
+              <ListItem primaryText={ this.props.title } disabled="true" />
             </List>
             <List subheader="Status" subheaderStyle={ SUBHEADERSTYLE }>
-              <ListItem primaryText="Saved to Google Drive" disabled={true} />
+              <ListItem primaryText="Saved to Google Drive" disabled="true" />
             </List>
             <List subheader="Domain" subheaderStyle={ SUBHEADERSTYLE }>
-              <ListItem primaryText={ this.props.originalUrl } disabled={true} />
+              <ListItem primaryText={ this.props.originalUrl } disabled="true" />
             </List>
             <List subheader="Folder" subheaderStyle={ SUBHEADERSTYLE }>
-              <ListItem primaryText={ this.props.folder } disabled={true} />
+              <ListItem primaryText={ this.props.folder } disabled="true" />
             </List>
-            <List subheader="Tags" subheaderStyle={ SUBHEADERSTYLE }>
+            <List subheader="Tags" subheaderStyle={ SUBHEADERSTYLE } >
+              <form onSubmit={this.submitForm}>
+              <TextField hintText="Add a tag" /><br/>
+                <FlatButton
+                  label="Add Tag" type="submit" hoverColor="WHITE" disableTouchRipple="true"
+                />
+              </form>
               <TagList currentTags={ this.props.currentTags } />
             </List>
           </div>
-
-
-
       </div>
-
-
     );
   }
 }
@@ -63,4 +70,4 @@ ImageInfo.propTypes = {
   originalUrl: React.PropTypes.string.isRequired,
 };
 
-export default ImageInfo
+export default ImageInfo;
