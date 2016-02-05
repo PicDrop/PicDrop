cdf4e44973cd5e07caff9670cf1c8f6ef41d40c3
Santosh Gautam <santoshgautam7@gmail.com>
2016-02-05 12:40:59 -0800

    Implemented form fields.

--- client/UserProfile/Profile.js
+++ client/UserProfile/Profile.js
@@ -2,14 +2,14 @@ import React from 'react';
 import { reduxForm } from 'redux-form';
 class Profile extends React.Component {
   render() {
-    const { handleSubmit } = this.props;
+    const { fields: { email, password }, handleSubmit } = this.props;
     return (
       <form onSubmit={handleSubmit}>
         <div>User Profile</div>
         <label>Email</label>
-        <input type="text" className=""/>
+        <input type="text" className="" {...email}/>
         <label>Password</label>
-        <input type="password" className="" placeholder="New Password"/>
+        <input type="password" className="" placeholder="New Password" {...password}/>
         <button>Cancel</button>
         <button type="submit">Save</button>
       </form>
