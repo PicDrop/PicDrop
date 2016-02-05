dba360b7d8143b0f054f3cbe9842f105429d4413
Travis Miller <travisbmiller@outlook.com>
2016-02-05 11:15:45 -0800

    updated auth controller

--- server/controllers/api/auth.controller.js
+++ server/controllers/api/auth.controller.js
@@ -13,18 +13,20 @@ function buildUserState(user){
   user.userPics.forEach(function(pic){
     var id = pic.id;
     newUser.userPics[id] = pic;
-    if(newUser.folder[pic.folder]) {
-      newUser.folder[pic.folder][id] = true;
+    if(newUser.folders[pic.folder]) {
+      newUser.folders[pic.folder][id] = true;
     } else {
-      newUser.folder[pic.folder] = { id: true };
+      newUser.folders[pic.folder] = { id: true };
     }
-    user.tags.forEach(function(tag){
-      if(newUser.tags[tag]){
-        newUser.tags[tag][id] = true;
-      } else {
-        newUser.tags[tag] = { id: true };
-      }
-    });
+    if (user.tags) {
+      user.tags.forEach(function(tag){
+        if(newUser.tags[tag]){
+          newUser.tags[tag][id] = true;
+        } else {
+          newUser.tags[tag] = { id: true };
+        }
+      });
+    }
     newUser.viewing.currentViewing.push(id);
   });
   return newUser;
