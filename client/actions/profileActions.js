894ab8fc821b489f4c7da3215adfa1030f0d21f7
Santosh Gautam <santoshgautam7@gmail.com>
2016-02-05 15:34:39 -0800

    Added action creator for profile actions.

--- client/actions/profileActions.js
+++ client/actions/profileActions.js
@@ -1,8 +1,25 @@
+import axios from 'axios';
+const ROOT_URL = '/some/url';
+
 const profileActions = {
   setState: (newState) => {
     return {
       type: 'SET_PROFILE_STATE',
       state: newState,
+    };
+  },
+  getProfile: () => {
+    const request = axios.get(`${ROOT_URL}/get`);
+    return {
+      type: 'GET_PROFILE',
+      payload: request,
+    };
+  },
+  setProfile: (props) => {
+    const request = axios.post(`${ROOT_URL}/post`, props);
+    return {
+      type: 'SET_PROFILE',
+      payload: request,
     };
   },
 };
