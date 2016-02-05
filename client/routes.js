ac7a89187c177226051b507c59fd95bb9b79d60a
Santosh Gautam <santoshgautam7@gmail.com>
2016-02-05 11:00:23 -0800

    Added route for profile.

--- client/routes.js
+++ client/routes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './App.js';
 import SplashView from './Splash/SplashView';
+import Profile from './UserProfile/Profile';
 import Main from './Main/Main';
 import Collection from './Main/Content/Images/Collection/Collection';
 import Image from './Main/Content/Images/Image/Image';
@@ -10,6 +11,7 @@ import Image from './Main/Content/Images/Image/Image';
 export default (
   <Route path="/" component={App}>
     <Route path="splash" component={SplashView} />
+    <Route path="profile" component={Profile} />
     <Route path="main" component={Main}>
       <Route path="collection" component={Collection} />
       <Route path="image/:imageId" component={Image} />
