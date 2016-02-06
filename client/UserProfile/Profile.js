a18e2702739e759b22b809e440d0c45ea340df01
Santosh Gautam <santoshgautam7@gmail.com>
2016-02-05 20:02:16 -0800

    Added initial styling to the form.

--- client/UserProfile/Profile.js
+++ client/UserProfile/Profile.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import TextField from 'material-ui/lib/text-field';
+import Paper from 'material-ui/lib/paper';
+import RaisedButton from 'material-ui/lib/raised-button';
 import profileActions from '../actions/profileActions';
 import { reduxForm } from 'redux-form';
 
@@ -7,16 +10,35 @@ class Profile extends React.Component {
     const { fields: { email, password }, handleSubmit } = this.props;
     console.log(this.props.setProfile)
     return (
-      <form onSubmit={handleSubmit(this.props.setProfile)}>
-        <div>User Profile</div>
-        <label>Email</label>
-        <input type="text" className="" {...email}/>
-        <label>Password</label>
-        <input type="password" className="" placeholder="New Password" {...password}/>
-        <div>{email.touched ? email.error : ''}, {password.touched ? password.error : ''}</div>
-        <button>Cancel</button>
-        <button type="submit">Save</button>
-      </form>
+      <div className="row">
+        <div className="col-sm-3 col-md-3 col-lg-2">
+          <Paper zDepth={1}>
+            <form onSubmit={handleSubmit(this.props.setProfile)}>
+              <h4>Edit Profile</h4>
+              <label>Email</label>
+              <TextField
+                defaultValue = "Default Email"
+                {...email}
+              />
+              <label>Password</label>
+              <TextField
+                type="password"
+                placeholder="New Password"
+                {...password}
+              />
+              <div>{email.touched ? email.error : ''}, {password.touched ? password.error : ''}</div>
+              <RaisedButton
+                label="Cancel"
+              />
+              <RaisedButton
+                label="Save"
+                disabled={true}
+                type="submit"
+              />
+            </form>
+          </Paper>
+        </div>
+      </div>
     );
   }
 }
