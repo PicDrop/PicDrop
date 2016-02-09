7aca0dd6d4cf9d43a61987db8f227aaaf5a683b8
Bryan Bierce <bryanbierce@gmail.com>
2016-02-09 10:49:32 -0800

    Added route for updating user password

--- server/controllers/api/user.controller.js
+++ server/controllers/api/user.controller.js
@@ -116,66 +116,18 @@ module.exports = {
       })
     });
   },
-  getCategory: function(req, res){},
-  getTagname: function(req, res){},
+  updatePassword: function(req, res){
+    DB.get(req.user.id).run().then(function(user){
+      if(req.body.oldPassword === user.password){
+        user.password = req.body.newPassword;
+        user.save().then(function(user){
+          res.status(201).send('Password update successfull');   
+        });
+      }else{
+        user.save().then(function(user){
+          res.status(401).send('Invalid old password');
+        });
+      }
+    });
+  }
 }  
-    // DB.User.get(req.user).getJoin({folders: true, userPics: true, tags: true}).run().then(function(user){
-    //   var newTags = [], newFolder;
-    //   var tags = req.body.tags, folder = req.body.folder;
-    //   var newPic = new DB.Picture({url: req.body.url, domain: req.body.domain});
-    //   for(var i = 0; i < tags.length; i++){
-    //     DB.Tag.filter({name: tags[i]}).run().then(function(tag){
-    //       if(tag.length){
-    //         newTags.push(tag[0]);
-    //       }else{
-    //         newTags.push( new DB.Tag({name: tags[i]}) );
-    //       }
-    //     });
-    //   }
-    //   DB.Folder.filter({name: folder}).run().then(function(folder){
-    //     if(folder.length){
-    //       newFolder = folder[0];
-    //     }else{
-    //       newFolder = new DB.Folder({name: folder});
-    //     }
-    //   });
-    //   newPic.addRelation('folder', newFolder);
-    //   newFolder.addRelation('pictures', newPic);
-    //   newFolder.addRelation('user', user);
-    //   for(var j=0; j < newTags.length; j++){
-    //     newPic.addRelation('tags', newTags[j]);
-    //     user.addRelation('tags', newTags[j]);
-    //     newTags[j].addRelation('pictures', newPic);
-    //     newTags[j].addRelation('user', user);
-    //   }
-    //   user.addRelation('folders', newFolder);
-    //   user.addRelation('userPics', newPic);
-    //   user.saveAll({userPics: true, folders: true, tags: true}).then(function(user){
-    //     res.status(201).send('Picture saved');
-    //   });
-    // });
-
-/*  tag drop
-*/
-    // DB.User.get(req.user).getJoin({tags: true}).run().then(function(user){
-    //   var newTag = null, pic;
-    //   DB.Picture.get(req.body.picId).run().then(function(picture){
-    //     pic = picture;
-    //   });
-    //   user.tags.forEach(function(tag){
-    //     if(tag.name === req.body.tag){
-    //       newTag = tag;
-    //     }
-    //   });
-    //   if(newTag === null){
-    //     newTag = new DB.tag
-    //     user.addRelation('tags', newTag);
-    //     newTag.addRelation('user', user);
-    //   }
-    //   newTag.addRelation('pictures', pic);
-    //   pic.addRelation('tags', newTag);
-    //   pic.save();
-    //   user.saveAll({tags: true}).then(function(user){
-    //     res.status(201).send("Added tag");
-    //   });
-    // })
