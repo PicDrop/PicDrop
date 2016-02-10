d9442ec5f432fe0695874b7eab7bca2c91bbceb3
Santosh Gautam <santoshgautam7@gmail.com>
2016-02-09 23:44:00 -0800

    Added basic shell for social.

---
+++ client/About/Social.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class Social extends React.Component {
+  render() {
+    return (
+      <div>
+        Social Icons
+      </div>
+    );
+  }
+}
+
+export default Social;
