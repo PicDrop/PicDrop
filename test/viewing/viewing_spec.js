d695a4bf0dc2d09d3067d01882b40b3bf5351e53
Santosh Gautam <santoshgautam7@gmail.com>
2016-02-09 21:51:27 -0800

    Added test for folder_selected.

--- test/viewing/viewing_spec.js
+++ test/viewing/viewing_spec.js
@@ -1,4 +1,4 @@
-import { Map, Set, fromJS } from 'immutable';
+import { Map, Set, List, fromJS } from 'immutable';
 import { expect } from 'chai';
 
 import reducer from '../../client/reducers/viewingReducer';
@@ -31,7 +31,39 @@ describe('reducer', () => {
   });
 
   it('handles FOLDER_SELECTED', () => {
-
-  })
-
+    const initialState = new Map({
+      username: 'testUser',
+      currentFolder: 'folder',
+      currentFolderPics: new Map(),
+      currentTags: new Set(),
+      currentTagsPics: new Map(),
+      currentCollection: new Set(),
+      currentImage: '1',
+    });
+    const action = {
+      type: 'FOLDER_SELECTED',
+      payload: {
+        folderName: 'myFolder',
+        folderObj: new Map({
+          f1: true,
+          f2: true,
+          f3: true,
+        }),
+      },
+    };
+    const nextState = reducer(initialState, action);
+    expect(nextState).to.equal(fromJS({
+      username: 'testUser',
+      currentFolder: 'myFolder',
+      currentFolderPics: new Map({
+        f1: true,
+        f2: true,
+        f3: true,
+      }),
+      currentTags: new Set(),
+      currentTagsPics: new Map(),
+      currentCollection: List.of('f1', 'f2', 'f3'),
+      currentImage: '1',
+    }));
+  });
 });
