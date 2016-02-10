3bb91be8a1f1cc8c9009f132878530a76e9df67e
Santosh Gautam <santoshgautam7@gmail.com>
2016-02-09 22:36:15 -0800

    Added initial shell and routing for about page.

---
+++ client/About/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        About us
+      </div>
+    );
+  }
+}
+
+export default About;
--- client/routes.js
+++ client/routes.js
@@ -7,10 +7,12 @@ import Main from './Main/Main';
 import Collection from './Main/Content/Images/Collection/Collection';
 import Image from './Main/Content/Images/Image/Image';
 import Stream from './Main/Content/Stream/Stream';
+import About from './About/About';
 
 export default (
   <Route path="/" component={App}>
     <Route path="splash" component={SplashView} />
+    <Route path="about" component={About} />
     <Route path="profile" component={Profile} />
     <Route path="main" component={Main}>
       <Route path="collection" component={Collection} />
