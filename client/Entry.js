e072a3f5ce81bc6e477cdd1b02c99d643ac1e718
Bryan Bierce <bryanbierce@gmail.com>
2016-02-13 20:30:27 -0800

    Reinstated dev tools and prepared for continuing development

--- client/Entry.js
+++ client/Entry.js
@@ -4,16 +4,19 @@ import { Router } from 'react-router';
 import { Provider } from 'react-redux';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import routes from './routes';
-import store from './store';
-// import initialState from './defaultState';
+import configStore from './store';
+import initialState from './defaultState';
 import DevTools from './devTools/devTools';
 
 injectTapEventPlugin();
-// const store = configStore(initialState);
+const store = configStore(initialState);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router >{ routes }</Router>
+    <div>
+      <DevTools />
+      <Router >{ routes }</Router>
+    </div>
   </Provider>,
   document.getElementById('app')
 );
--- server/utils/thinky.js
+++ server/utils/thinky.js
@@ -1,5 +1,5 @@
 var options = {
-  host: process.env.rethinkURI || '162.243.7.79'
+  host: process.env.rethinkURI || '127.0.0.1'
 };
 
 module.exports = require('thinky')({ host: options.host});
