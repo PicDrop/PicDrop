00beb8230ae643a878e901c9fdb23352a00ef956
Ryan Jagger <ryanjagger@gmail.com>
2016-02-19 10:00:37 -0600

    Cleaned spacing/syntax

--- client/Splash/SplashView.js
+++ client/Splash/SplashView.js
@@ -7,21 +7,6 @@ class SplashView extends React.Component {
     return (
       <div id="splashview">
         <div className="intro-header">
-<<<<<<< Updated upstream
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <div className="intro-message">
-                            <h1>PICDROP</h1>
-                            <h3>Save the photos you love, organize them your way.</h3>
-                            <ul className="list-inline intro-social-buttons">
-                              <li>
-                                  <FlatButton label="Github" href={"https://github.com/PicDrop/PicDrop"} backgroundColor="white" primary={true} />
-                              </li>
-                            </ul>
-                        </div>
-                    </div>
-=======
           <div className="container">
             <div className="row">
               <div className="col-lg-12">
@@ -38,50 +23,31 @@ class SplashView extends React.Component {
                       />
                     </li>
                   </ul>
->>>>>>> Stashed changes
                 </div>
               </div>
             </div>
           </div>
         </div>
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
         <div className="content-section-a">
           <div className="container">
             <div className="row">
               <div className="col-lg-5 col-sm-6">
                 <div className="clearfix"></div>
-                  <h2 className="section-heading">Easily tag and create categories right from your browser</h2>
-                  <p className="lead"></p>
+                <h2 className="section-heading">Easily tag and create categories right from your browser</h2>
+                <p className="lead"></p>
               </div>
-              <div className="col-lg-5 col-lg-offset-2 col-sm-6">
-                <img className="img-responsive"
-                  style={{ width: '100%', height: 'auto' }}
-                  src="assets/select-closeup.png"
-                  alt=""
-                />
-              </div>
+                <div className="col-lg-5 col-lg-offset-2 col-sm-6">
+                  <img
+                    className="img-responsive"
+                    style={{ width: '100%', height: 'auto' }}
+                    src="assets/select-closeup.png"
+                    alt=""
+                  />
+                </div>
             </div>
           </div>
         </div>
         <div className="content-section-b">
-<<<<<<< Updated upstream
-            <div className="container">
-                <div className="row">
-                  <div className="col-lg-5 col-sm-pull-6  col-sm-6">
-                    <video preload="auto" autoPlay loop="loop" muted="muted" volume="0" id="myVideo">
-                      <source src="assets/video.mp4" type="video/mp4" />
-                    </video>
-                  </div>
-                  <div className="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
-                    <div className="clearfix"></div>
-                    <h2 className="section-heading">Add tags to find it later</h2>
-                    <p className="lead"></p>
-                  </div>
-                </div>
-=======
           <div className="container">
             <div className="row">
               <div className="col-lg-5 col-sm-pull-6  col-sm-6">
@@ -89,12 +55,11 @@ class SplashView extends React.Component {
                   <source src="assets/video.mp4" type="video/mp4" />
                 </video>
               </div>
-              <div className="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
-                <div className="clearfix"></div>
-                <h2 className="section-heading">Add tags to find it later</h2>
-                <p className="lead"></p>
-              </div>
->>>>>>> Stashed changes
+                <div className="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
+                    <div className="clearfix"></div>
+                    <h2 className="section-heading">Add tags to find it later</h2>
+                    <p className="lead"></p>
+                </div>
             </div>
           </div>
         </div>
@@ -103,26 +68,20 @@ class SplashView extends React.Component {
             <div className="row">
               <div className="col-lg-5 col-sm-6">
                 <div className="clearfix"></div>
-                  <h2 className="section-heading">View your pictures in your way</h2>
-                  <p className="lead"></p>
-                </div>
-<<<<<<< Updated upstream
-            </div>
-        </div>
-
-=======
-                  <div className="col-lg-5 col-lg-offset-2 col-sm-6">
-                    <img
-                      className="img-responsive"
-                      style={{ width: '100%', height: 'auto' }}
-                      src="assets/extension1.png"
-                      alt=""
-                    />
-                </div>
+                <h2 className="section-heading">View your pictures in your way</h2>
+                <p className="lead"></p>
               </div>
+                <div className="col-lg-5 col-lg-offset-2 col-sm-6">
+                  <img
+                    className="img-responsive"
+                    style={{ width: '100%', height: 'auto' }}
+                    src="assets/extension1.png"
+                    alt=""
+                  />
+                </div>
             </div>
           </div>
->>>>>>> Stashed changes
+        </div>
         <footer>
           <div className="container">
             <div className="row">
