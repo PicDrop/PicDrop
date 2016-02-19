ee40a0a27dd8ea35a709fd5ce102a71e28568fc5
Ryan Jagger <ryanjagger@gmail.com>
2016-02-19 09:33:40 -0600

    Cleaned spacing/syntax

--- client/Splash/SplashView.js
+++ client/Splash/SplashView.js
@@ -7,6 +7,7 @@ class SplashView extends React.Component {
     return (
       <div id="splashview">
         <div className="intro-header">
+<<<<<<< Updated upstream
             <div className="container">
                 <div className="row">
                     <div className="col-lg-12">
@@ -20,26 +21,53 @@ class SplashView extends React.Component {
                             </ul>
                         </div>
                     </div>
+=======
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <div className="intro-message">
+                  <h1>PICDROP</h1>
+                  <h3>Save the photos you love, organize them your way.</h3>
+                  <ul className="list-inline intro-social-buttons">
+                    <li>
+                      <FlatButton
+                        label="Github"
+                        href={"https://github.com/PicDrop/PicDrop"}
+                        backgroundColor="white"
+                        primary={true}
+                      />
+                    </li>
+                  </ul>
+>>>>>>> Stashed changes
                 </div>
+              </div>
             </div>
+          </div>
         </div>
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
         <div className="content-section-a">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-5 col-sm-6">
-                        <div className="clearfix"></div>
-                        <h2 className="section-heading">Easily tag and create categories right from your browser</h2>
-                        <p className="lead"></p>
-                    </div>
-                    <div className="col-lg-5 col-lg-offset-2 col-sm-6">
-                        <img className="img-responsive" style={{ width: '100%', height: 'auto' }} src="assets/select-closeup.png" alt="" />
-                    </div>
-                </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-5 col-sm-6">
+                <div className="clearfix"></div>
+                  <h2 className="section-heading">Easily tag and create categories right from your browser</h2>
+                  <p className="lead"></p>
+              </div>
+              <div className="col-lg-5 col-lg-offset-2 col-sm-6">
+                <img className="img-responsive"
+                  style={{ width: '100%', height: 'auto' }}
+                  src="assets/select-closeup.png"
+                  alt=""
+                />
+              </div>
             </div>
+          </div>
         </div>
-
         <div className="content-section-b">
+<<<<<<< Updated upstream
             <div className="container">
                 <div className="row">
                   <div className="col-lg-5 col-sm-pull-6  col-sm-6">
@@ -53,35 +81,58 @@ class SplashView extends React.Component {
                     <p className="lead"></p>
                   </div>
                 </div>
+=======
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-5 col-sm-pull-6  col-sm-6">
+                <video preload="auto" autoPlay loop="loop" muted="muted" volume="0" id="myVideo">
+                  <source src="assets/video.mp4" type="video/mp4" />
+                </video>
+              </div>
+              <div className="col-lg-5 col-lg-offset-1 col-sm-push-6  col-sm-6">
+                <div className="clearfix"></div>
+                <h2 className="section-heading">Add tags to find it later</h2>
+                <p className="lead"></p>
+              </div>
+>>>>>>> Stashed changes
             </div>
+          </div>
         </div>
-
         <div className="content-section-a">
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-5 col-sm-6">
-                        <div className="clearfix"></div>
-                        <h2 className="section-heading">View your pictures in your way</h2>
-                        <p className="lead"></p>
-                    </div>
-                    <div className="col-lg-5 col-lg-offset-2 col-sm-6">
-                        <img className="img-responsive" style={{ width: '100%', height: 'auto' }} src="assets/extension1.png" alt="" />
-                    </div>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-5 col-sm-6">
+                <div className="clearfix"></div>
+                  <h2 className="section-heading">View your pictures in your way</h2>
+                  <p className="lead"></p>
                 </div>
+<<<<<<< Updated upstream
             </div>
         </div>
 
-        <footer>
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-12">
-                        <p className="copyright text-muted small">Copyright &copy; PicDrop 2016. All Rights Reserved</p>
-                    </div>
+=======
+                  <div className="col-lg-5 col-lg-offset-2 col-sm-6">
+                    <img
+                      className="img-responsive"
+                      style={{ width: '100%', height: 'auto' }}
+                      src="assets/extension1.png"
+                      alt=""
+                    />
                 </div>
+              </div>
             </div>
+          </div>
+>>>>>>> Stashed changes
+        <footer>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12">
+                <p className="copyright text-muted small">Copyright &copy; PicDrop 2016. All Rights Reserved</p>
+              </div>
+            </div>
+          </div>
         </footer>
       </div>
-
     );
   }
 }
